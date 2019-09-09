296045b85597a755a8e0289a0cbb3538faabab81
anami823 <anami823@gmail.com>
2019-09-09 10:39:58 -0700

    Crate base template

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the Cat Collector</h1>')
+  return render(request, 'about.html')
